a6bc1fccd494c474635325106919df1be8244b60
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-10-16 01:43:53 +0100

    Centered Navbar Items on Small Screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,30 +70,34 @@ const Navbar = ({ itemCount }) => {
         </div>
       </div>
 
-      {/* Mobile navigation */}
-      {isMenuOpen && (
-        <div className="lg:hidden mt-4 bg-gray-500">
-          <nav className="p-1 rounded-full bg-slate-500 bg-opacity-15">
-            <ul className="flex flex-col space-y-2">
-              {navItems.map((item) => (
-                <NavItem key={item.name} item={item} />
-              ))}
-            </ul>
-          </nav>
-          <div className="flex justify-center mt-4 space-x-2">
-            <div className='relative'>
-              <Link to='/shop/cart'>
-                <ShoppingCart size={40} className="bg-slate-500 bg-opacity-15 text-white rounded-full p-2 hover:bg-slate-800" />
-              </Link>
-              <p className='absolute text-white top-0 right-0 bg-blue-500 rounded-full h-5 w-5 text-xs flex items-center justify-center'>
-                {itemCount}
-              </p>
-            </div>
-            <button className="bg-slate-500 bg-opacity-15 text-white rounded-full p-2 hover:bg-slate-800">Login</button>
-            <button className="bg-slate-500 bg-opacity-15 text-white rounded-full p-2 hover:bg-slate-800">Sign up</button>
+     {/* Mobile navigation */}
+    {isMenuOpen && (
+      <div className="lg:hidden mt-4 bg-gray-900">
+        {/* Centered navigation items */}
+        <nav className="p-1 rounded-full  bg-opacity-15">
+          <ul className="flex flex-col items-center space-y-2"> {/* items-center to center horizontally */}
+            {navItems.map((item) => (
+              <NavItem key={item.name} item={item} />
+            ))}
+          </ul>
+        </nav>
+
+        {/* Centered cart and buttons */}
+        <div className="flex flex-col items-center mt-4 space-y-2"> {/* Flex column to stack items and center them */}
+          <div className="relative">
+            <Link to='/shop/cart'>
+              <ShoppingCart size={40} className="bg-slate-500 bg-opacity-15 text-white rounded-full p-2 hover:bg-slate-800" />
+            </Link>
+            <p className="absolute text-white top-0 right-0 bg-blue-500 rounded-full h-5 w-5 text-xs flex items-center justify-center">
+              {itemCount}
+            </p>
           </div>
+          <button className="bg-slate-500 bg-opacity-15 text-white rounded-full p-2 hover:bg-slate-800">Login</button>
+          <button className="bg-slate-500 bg-opacity-15 text-white rounded-full p-2 hover:bg-slate-800">Sign up</button>
         </div>
-      )}
+      </div>
+    )}
+
     </div>
   );
 };
